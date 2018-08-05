0ae4be80ae99756a62d39060c3fbe958d41c74e4
Kevin Davis <dawnpatrol506@gmail.com>
2018-08-05 15:05:40 -0700

    starting game logic, page mockup done

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var rocky_words = ["rocky", "adrian", "paulie", "boxing", "thunderlips", "clubber", "russian",
+"homoerotic", "montage", "roboservant", "worse-than-short-round"];
+
+var word = "";
+var guesses = [];
+
+document.addEventListener("keypress", function(event){
+    var key = String.fromCharCode(event.which);
+
+    
+});
